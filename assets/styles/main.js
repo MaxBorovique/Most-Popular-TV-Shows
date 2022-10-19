1709e4edfe81b10090bd79ea707fe51b2952bdd1
MaxBorovique <Max.borovique@gmail.com>
2022-10-19 22:58:04 +0300

    Add slider

---
+++ assets/styles/main.js
@@ -0,0 +1,12 @@
+$('.slider').slick({
+    slidesToScroll: 1,
+    slidesToShow: 4,
+    dots: false,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: '<img src="/assets/images/see_also_images/arrow_r.svg" alt="">',
+    prevArrow: '<img src="/assets/images/see_also_images/arrow_l.svg" alt="">',
+    
+});
+
